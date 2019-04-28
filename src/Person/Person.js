45ceb61b271d5fd72468661ab0001eeb2c369710
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-04-27 17:36:05 -0700

    first commit

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const person = (props) => {
+  return (
+    <div onClick={props.click}>
+      <p>I am {props.name} and I am {props.age} years old</p>
+      <p>{props.children}</p>
+    </div>
+  )
+}
+
+export default person
