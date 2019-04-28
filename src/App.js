cff66351b70877df720688b1e7a6d9f6cec9a24b
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-04-27 17:42:21 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Justin', age: 33 },
+      { name: 'Angela', age: 28 },
+      { name: 'Alex', age: 34 }
+    ],
+  })
+
+  const [otherState, setOtherState] = useState("some other value")
+  console.log(personsState, otherState)
+
+  const nameHandler = (newName) => {
+    // console.log('was clicked')
+    setPersonsState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Jordan', age: 28 },
+        { name: 'Jesus', age: 34 }
+      ],
+    })
+    setOtherState("this is a new value!")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi! I am a React App</h1>
+      <p>This is really working!!</p>
+      <button onClick={nameHandler.bind(this, "Justin!!!")}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        click={nameHandler.bind(this, "JJJ")} >Hobbies: Hiking</Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
     </div>
   );
-}
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello! I am a React App'))
+  }
 
 export default App;
